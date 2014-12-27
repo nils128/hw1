d6b3b813ea70d30780ebe7a4388d673f5363c6f6
Nils Kellermann <nils.kellermann@mni.thm.de>
2014-12-27 13:43:02 +0100

    commit

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
-<?php
-echo "Dies hier ist ein Test von Tim Kellermann."
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Beispielseite</title>
+<style>
+#footer {
+    clear:left;
+    margin-top:1em;
+    background:#efefef;
+    text-align:center;
+    padding:10px;
+	position:fixed;
+    bottom:0px;
+}
 
-?>
+#content_container {
+    padding-bottom:3em;
+}
+</style>
+</head>
+<body>
+<body background="bg.png">
+
+<div id="content-container"><h1>Beispielhomepage</h1>
+
+<p>Eine simple HTML-Seite
+</p>
+</div>
+<div id="footer">
+<h1>Impressum</h1>
+
+<p>Tim Kellermann <br>
+Frankenstra&szlig;e 12 <br>
+35510 Pohl-G&ouml;ns <br>
+timsaccount@web.de <br>
+06033 73444
+</p>
+</div>
+</body>
+</html>
